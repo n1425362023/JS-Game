6afcdb87e465702ddcd7ea779afbd5da694859f6
n1425362023 <147420390+n1425362023@users.noreply.github.com>
2024-02-14 18:38:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ window.addEventListener('load',function(){
         //敌人生成
         addEnemy(){
             if(Math.random()<0.2)this.enemies.push(new GroundEnemy(this));
-            else if(Math.random()<0.1&&this.score<50) this.enemies.push(new ZombieEnemy(this));
+            else if(Math.random()<0.1&&this.score<100) this.enemies.push(new ZombieEnemy(this));
             if(Math.random()<0.5)this.enemies.push(new FlyingEnemy(this));
             if(Math.random()<0.5)this.enemies.push(new BossGhost(this));
             //console.log(game.enemies);
